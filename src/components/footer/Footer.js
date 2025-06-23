0341898094bc05e5ca201d4c11cc7c4022f487ba
STEVEN_HOSKINS <steveo.hozzy@gmail.com>
2025-06-23 10:51:34 +0100

    footer links in place

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -55,7 +55,7 @@ const Footer = () => {
                 href="http://instagram.com"
                 target="_blank"
                 rel="noreferrer"
-                className="ml-1 transition-all hover:rotate-[20deg]"
+                className="ml-2 transition-all hover:rotate-[20deg]"
               >
                 <svg
                   width="16"
@@ -72,7 +72,7 @@ const Footer = () => {
               </a>
             </div>
             <div className="flex items-start justify-center">
-              <div className="px-4">
+              <div className="px-2">
                 <button
                   onClick={() => setAccordionOpen(!accordionOpen)}
                   className="text-brandBlue font-bold flex items-center"
@@ -153,7 +153,7 @@ const Footer = () => {
                   </div>
                 </div>
               </div>
-              <div className="px-4">
+              <div className="px-2">
                 <button
                   onClick={() => setAccordionOpen(!accordionOpen)}
                   className="text-brandBlue font-bold flex items-center"
@@ -233,6 +233,101 @@ const Footer = () => {
                     </ul>
                   </div>
                 </div>
+              </div>
+              <div className="px-2">
+                <button
+                  onClick={() => setAccordionOpen(!accordionOpen)}
+                  className="text-brandBlue font-bold flex items-center"
+                >
+                  <span className="mr-2">Links </span>
+                  <span
+                    className={`w-0 h-0 border-l-[8px] border-l-transparent border-r-[8px] border-r-transparent border-t-[8px] border-brandBlue transition-all ${
+                      accordionOpen && "rotate-180"
+                    }`}
+                  ></span>
+                </button>
+
+                <div
+                  className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600 text-sm ${
+                    accordionOpen
+                      ? "grid-rows-[1fr] opacity-100"
+                      : "grid-rows-[0fr] opacity-0"
+                  }`}
+                >
+                  <div className="overflow-hidden">
+                    <ul className="text-brandBlue">
+                      <li className="mt-2">
+                        <a
+                          href="https://theentertainer.zendesk.com/hc/en-gb/articles/6495305266833-Contact-Us"
+                          title="Contact Us"
+                        >
+                          Contact Us
+                        </a>
+                      </li>
+                      <li className="mt-2">
+                        <a
+                          href="https://theentertainer.zendesk.com/hc/en-gb/articles/6480509734289-Delivery-information"
+                          title="Delivery Options"
+                        >
+                          Delivery Options
+                        </a>
+                      </li>
+                      <li className="mt-2">
+                        <a href="/help" title="Help Centre">
+                          Help Centre
+                        </a>
+                      </li>
+                      <li className="mt-2">
+                        <a
+                          href="https://www.thetoyshop.com/product-safety-notices"
+                          title="Product Safety Notices"
+                        >
+                          Product Safety
+                        </a>
+                      </li>
+                      <li className="mt-2">
+                        <a
+                          href="https://theentertainer.zendesk.com/hc/en-gb/articles/4402417396241-Our-Returns-Policy"
+                          title="Returns"
+                        >
+                          Returns
+                        </a>
+                      </li>
+                      <li className="mt-2">
+                        <a href="/my-account/orders" title="Track Your Order">
+                          Track Your Order
+                        </a>
+                      </li>
+                      <li className="mt-2">
+                        <a href="/privacy" title="Your Privacy">
+                          Your Privacy
+                        </a>
+                      </li>
+                      <li className="mt-2">
+                        <a
+                          href="https://theentertainer.zendesk.com/hc/en-gb/articles/32765165894801-How-to-complain"
+                          title="How To Complain"
+                        >
+                          How To Complain
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="px-2">
+                <a href="/klarna"
+                  className="text-brandBlue font-bold"
+                >
+                  <span>Links </span>
+                </a>
+              </div>
+              <div className="px-2">
+                <a href="/klarna"
+                  className="text-brandBlue font-bold"
+                >
+                  <span>Events </span>
+                </a>
               </div>
             </div>
           </div>
