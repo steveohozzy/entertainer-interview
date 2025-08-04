55eec810b1efab4a2483112c43e1a823a4db1134
STEVEN_HOSKINS <steve.hoskins@theentertainer.com>
2025-08-04 08:09:05 +0100

    adding sizes to see if it then shows on iPhone

--- src/components/wishlistContainer/WishlistContainer.js
+++ src/components/wishlistContainer/WishlistContainer.js
@@ -53,6 +53,8 @@ const WishlistContainer = () => {
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
                   fill="none"
                 >
                   <path
@@ -67,7 +69,7 @@ const WishlistContainer = () => {
               </span>
             <button className="transition-all border-[3px] border-brandRed rounded-full w-10 h-10 md:w-12 md:h-12 p-1 md:p-2 flex items-center justify-center hover:rotate-[45deg]">
               <span className="flex items-center w-9 h-9 text-brandRed">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none">
                   <path d="M15.4306 7.70172C7.55045 7.99826 3.43929 15.232 2.17021 19.3956C2.07701 19.7014 2.31139 20 2.63107 20C2.82491 20 3.0008 19.8828 3.08334 19.7074C6.04179 13.4211 12.7066 12.3152 15.514 12.5639C15.7583 12.5856 15.9333 12.7956 15.9333 13.0409V15.1247C15.9333 15.5667 16.4648 15.7913 16.7818 15.4833L20.6976 11.6784C20.8723 11.5087 20.8993 11.2378 20.7615 11.037L16.8456 5.32965C16.5677 4.92457 15.9333 5.12126 15.9333 5.61253V7.19231C15.9333 7.46845 15.7065 7.69133 15.4306 7.70172Z" fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
               </span>
