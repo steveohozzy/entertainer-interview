0597977e14818c7ea187b63132e7184da96ba179
STEVEN_HOSKINS <steve.hoskins@theentertainer.com>
2025-08-07 15:38:04 +0100

    showed exampe of not achieved acount badge

--- src/components/meAccountSection/MeAccountSection.js
+++ src/components/meAccountSection/MeAccountSection.js
@@ -7,12 +7,14 @@ const MeAccountSection = () => {
     <>
     <div className="flex gap-6 flex-wrap md:flex-nowrap items-start">
       <div className="flex items-center justify-between w-full flex-wrap md:w-2/3">
-        <div className="flex flex-col w-[100px]">
+        <div className="flex flex-col w-[100px] md:w-[150px]">
           <div className="text-lg font-bold md:text-xl text-textBlue">
             Account
           </div>
-          <div className="w-full bg-brandLightGreen rounded-full h-2"></div>
-          <div className="text-xs text-textBlue">100%</div>
+          <div className="w-full bg-gray-300 rounded-full h-2">
+            <div className="w-full bg-brandLightGreen rounded-full h-2 w-3/4"></div>
+          </div>
+          <div className="text-xs text-textBlue">75%</div>
         </div>
         <div className="flex items-center gap-2 w-[200px]">
           <button className="group relative">
@@ -50,6 +52,7 @@ const MeAccountSection = () => {
           </button>
           <button className="group relative">
             <img src="/account-icon-4.png" alt="account4" />
+            <div className="absolute bg-gray-500 bg-opacity-65 w-full h-full top-0 left-0 rounded-full"></div>
              <div className="transition-all absolute top-[-70px] left-0 opacity-0 group-hover:opacity-100 z-[999] pointer-events-none">
               <div className="relative bg-white border-[2px] w-[80px] p-3 text-xs text-textBlue border-brandLightBlue rounded-full before:content-[''] before:absolute before:top-[calc(100%-1px)] before:left-[23px] after:content-[''] after:absolute before:border-[12px] before:border-transparent after:border-[10px] after:border-transparent after:border-[10px] after:top-[calc(100%-1px)] after:left-[25px] after:border-t-white rotate-[10deg] before:!border-t-brandLightBlue">
                 <span className="block rotate-[-10deg] flex flex-col leading-[1.2]">
