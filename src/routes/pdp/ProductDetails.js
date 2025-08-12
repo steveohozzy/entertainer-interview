2c73f2edb1f17b856472f45834f970d484f7f903
STEVEN_HOSKINS <steve.hoskins@theentertainer.com>
2025-08-12 09:58:51 +0100

    timeout for add all to see if it stops it disssapearing

--- src/routes/pdp/ProductDetails.js
+++ src/routes/pdp/ProductDetails.js
@@ -82,10 +82,12 @@ const ProductDetails = () => {
           .click();
       }
     });
-    document.body.classList.add("body-noscroll");
-    window.scrollBy(0, -2);
-    dispatch(setIsCartOpen(true));
-  };
+    setTimeout(() => {
+      document.body.classList.add("body-noscroll");
+      window.scrollBy(0, -2);
+      dispatch(setIsCartOpen(true));
+    }, 500);
+    };
 
   // Add to favourites
   const addProductToWishlist = () => {
