2c7d2de43eb1a712d720a1b476403ccbeed5f2e0
STEVEN_HOSKINS <steve.hoskins@theentertainer.com>
2025-08-18 14:27:57 +0100

    room for hover effect

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
       <HomeAgeRange />
       <HomeCarousel />
       <div className='flex justify-center'>
-        <h3 id="toy-types" className="text-2xl md:text-3xl lg:text-4xl font-bold bg-gradient-to-r from-brandBlue via-textBlue to-brandBlue bg-clip-text md:!leading-[1.2] text-transparent text-center my-5 md:my-8">Toy Types</h3>
+        <h3 id="toy-types" className="text-2xl md:text-3xl lg:text-4xl font-bold bg-gradient-to-r from-brandBlue via-textBlue to-brandBlue bg-clip-text md:!leading-[1.2] text-transparent text-center mt-5 mb-2 mt-8 mb-5">Toy Types</h3>
       </div>
       <HomeToyTypes />
       <div className='flex justify-center'>
