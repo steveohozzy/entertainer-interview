ac50d1d5e07923a62f7cc7e06421a5ab6c753400
STEVEN_HOSKINS <steve.hoskins@theentertainer.com>
2025-08-18 08:47:51 +0100

    logo larger on mobile

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -112,7 +112,7 @@ const Layout = () => {
                     <span className="sr-only">View your favourites</span>
                 </button>
               </div>
-              <div className="flex items-center space-x-4 pt-2 ml-2 md:pt-4 md:ml-0 group">
+              <div className="flex items-center space-x-4 pt-2 ml-1 md:pt-4 md:ml-0 group">
                 <button onClick={() => {
                   navigate('/');
                    window.scrollTo({
@@ -124,12 +124,12 @@ const Layout = () => {
                   <img
                     src="/jack-logo.svg"
                     alt="The Entertainer"
-                    className="w-[39px] md:w-[59px] transition-all translate-y-2 group-hover:translate-y-0 group-hover:scale-105"
+                    className="w-[45px] md:w-[59px] transition-all translate-y-2 group-hover:translate-y-0 group-hover:scale-105"
                   />
                   <img
                     src="/text-logo.svg"
                     alt="The Entertainer"
-                    className="w-[95px] md:w-[154px] "
+                    className="w-[101px] md:w-[154px] "
                   />
                 </button>
               </div>
