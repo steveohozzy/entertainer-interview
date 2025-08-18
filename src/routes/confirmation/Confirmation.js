7f246b7d50798e98af3f911a85e334cda023ab14
STEVEN_HOSKINS <steve.hoskins@theentertainer.com>
2025-08-18 10:33:06 +0100

    forms better sizing and prodyct tile name size

--- src/routes/confirmation/Confirmation.js
+++ src/routes/confirmation/Confirmation.js
@@ -144,7 +144,7 @@ const Confirmation = () => {
                         <div className="flex flex-wrap justify-center">
                               <div className="inline-flex items-center text-xs text-gray-400">{product.brand}</div>
                           </div>
-                        <div className="text-xs lg:text-sm xl:text-lg text-brandBlue font-bold leading-[1.2] xl:leading-[1.1] mb-2 md:mb-0 line-clamp-2">
+                        <div className="text-xs lg:text-sm text-brandBlue font-bold leading-[1.2] xl:leading-[1.1] mb-2 md:mb-0 line-clamp-2">
                           {product.name}
                         </div>
                         <div>
