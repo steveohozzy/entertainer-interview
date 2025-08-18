5ca453522bce38b9c6c31daa49621d362e1fb61c
STEVEN_HOSKINS <steve.hoskins@theentertainer.com>
2025-08-18 10:55:30 +0100

    better spacing on larger screens in search area

--- src/components/searchBar/byTypeCarousel.js
+++ src/components/searchBar/byTypeCarousel.js
@@ -9,7 +9,7 @@ const ByTypeCarousel = ({setShowSearchBox}) => {
   const navigate = useNavigate();
 
   return (
-    <div>
+    <div className='md:mt-4'>
       <div className="text-lg text-textBlue font-bold mb-2">
         By Type
       </div>
