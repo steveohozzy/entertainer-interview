d7cbd5d23be9769c3a0c76fb1620c87122245691
STEVEN_HOSKINS <steve.hoskins@theentertainer.com>
2025-08-18 13:19:10 +0100

    font size adjust of titles on home

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -14,16 +14,16 @@ const Home = () => {
       <HomeAgeRange />
       <HomeCarousel />
       <div className='flex justify-center'>
-        <h3 id="toy-types" className="text-2xl md:text-4xl font-bold bg-gradient-to-r from-brandBlue via-textBlue to-brandBlue bg-clip-text md:!leading-[1.2] text-transparent text-center my-5 md:my-8">Toy Types</h3>
+        <h3 id="toy-types" className="text-2xl md:text-3xl lg:text-4xl font-bold bg-gradient-to-r from-brandBlue via-textBlue to-brandBlue bg-clip-text md:!leading-[1.2] text-transparent text-center my-5 md:my-8">Toy Types</h3>
       </div>
       <HomeToyTypes />
       <div className='flex justify-center'>
-        <h3 id="offers" className="text-2xl md:text-4xl font-bold bg-gradient-to-r from-brandBlue via-textBlue to-brandBlue bg-clip-text md:!leading-[1.2] text-transparent text-center my-5 md:my-8">Offers</h3>
+        <h3 id="offers" className="text-2xl md:text-3xl lg:text-4xl font-bold bg-gradient-to-r from-brandBlue via-textBlue to-brandBlue bg-clip-text md:!leading-[1.2] text-transparent text-center my-5 md:my-8">Offers</h3>
       </div>
       <HomeOffers />
       <HomeTopPicks />
       <div className='flex justify-center'>
-        <h3 id="brands" className="text-2xl md:text-4xl font-bold bg-gradient-to-r from-brandBlue via-textBlue to-brandBlue bg-clip-text md:!leading-[1.2] text-transparent text-center mb-8 mt-8 md:mt-12">Best Brands</h3>
+        <h3 id="brands" className="text-2xl md:text-3xl lg:text-4xl font-bold bg-gradient-to-r from-brandBlue via-textBlue to-brandBlue bg-clip-text md:!leading-[1.2] text-transparent text-center mb-8 mt-8 md:mt-12">Best Brands</h3>
       </div>
       <HomeBrands />
       <HomeHeroCard />
