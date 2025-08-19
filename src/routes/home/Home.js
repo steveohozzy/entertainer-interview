422189b0abc9e8a1b073d3e793f5aec90ca6391e
STEVEN_HOSKINS <steve.hoskins@theentertainer.com>
2025-08-19 08:14:24 +0100

    reduced 3d text effect

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -14,16 +14,16 @@ const Home = () => {
       <HomeAgeRange />
       <HomeCarousel />
       <div className='flex justify-center'>
-        <h3 id="toy-types" className="text-2xl md:text-3xl lg:text-4xl font-bold bg-gradient-to-r from-brandBlue via-textBlue to-brandBlue bg-clip-text md:!leading-[1.2] text-transparent text-center mt-5 mb-2 mt-8 mb-5 drop-shadow-[0_3px_0_rgba(255,255,255,1)]">Toy Types</h3>
+        <h3 id="toy-types" className="text-2xl md:text-3xl lg:text-4xl font-bold bg-gradient-to-r from-brandBlue via-textBlue to-brandBlue bg-clip-text md:!leading-[1.2] text-transparent text-center mt-5 mb-2 mt-8 mb-5 drop-shadow-[0_2px_0_rgba(255,255,255,1)]">Toy Types</h3>
       </div>
       <HomeToyTypes />
       <div className='flex justify-center'>
-        <h3 id="offers" className="text-2xl md:text-3xl lg:text-4xl font-bold bg-gradient-to-r from-brandBlue via-textBlue to-brandBlue bg-clip-text md:!leading-[1.2] text-transparent text-center my-5 md:my-8 drop-shadow-[0_3px_0_rgba(255,255,255,1)]">Offers</h3>
+        <h3 id="offers" className="text-2xl md:text-3xl lg:text-4xl font-bold bg-gradient-to-r from-brandBlue via-textBlue to-brandBlue bg-clip-text md:!leading-[1.2] text-transparent text-center my-5 md:my-8 drop-shadow-[0_2px_0_rgba(255,255,255,1)]">Offers</h3>
       </div>
       <HomeOffers />
       <HomeTopPicks />
       <div className='flex justify-center'>
-        <h3 id="brands" className="text-2xl md:text-3xl lg:text-4xl font-bold bg-gradient-to-r from-brandBlue via-textBlue to-brandBlue bg-clip-text md:!leading-[1.2] text-transparent text-center mb-8 mt-8 md:mt-12 drop-shadow-[0_3px_0_rgba(255,255,255,1)]">Best Brands</h3>
+        <h3 id="brands" className="text-2xl md:text-3xl lg:text-4xl font-bold bg-gradient-to-r from-brandBlue via-textBlue to-brandBlue bg-clip-text md:!leading-[1.2] text-transparent text-center mb-8 mt-8 md:mt-12 drop-shadow-[0_2px_0_rgba(255,255,255,1)]">Best Brands</h3>
       </div>
       <HomeBrands />
       <HomeHeroCard />
