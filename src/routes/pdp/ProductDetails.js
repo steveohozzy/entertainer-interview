7b8b451b2e9e3031684d91feb805ef9e0650bcc7
STEVEN_HOSKINS <steve.hoskins@theentertainer.com>
2025-08-19 13:13:14 +0100

    badges to design

--- src/routes/pdp/ProductDetails.js
+++ src/routes/pdp/ProductDetails.js
@@ -235,6 +235,18 @@ const ProductDetails = () => {
                 </SwiperSlide>
               ))}
             </Swiper>
+            <div className="absolute top-2 left-2 flex w-full gap-1 flex items-center drop-shadow-md z-20">
+              {product.isNew && (
+                <div className="inline-flex items-center rounded-lg px-2 py-0.5 text-xs md:text-sm w-12 h-12 md:w-14 md:h-14 text-center !leading-[1.1] font-bold bg-brandGreen text-white aspect-square">
+                  <span className="shadow-text-green">NEW TOYS</span>
+                </div>
+              )}
+              {product.isBestseller && (
+                <div className="inline-flex items-center rounded-lg px-2 py-0.5 text-xs md:text-sm w-12 h-12 md:w-14 md:h-14 text-center !leading-[1.1] font-bold bg-brandRed text-white aspect-square">
+                  <span className="shadow-text-red -mt-1.5"><span className="text-base md:text-lg">33</span>% OFF</span>
+                </div>
+              )}
+            </div>
             <div className="absolute top-3 right-3 z-[1]">
               <button
                 onClick={
