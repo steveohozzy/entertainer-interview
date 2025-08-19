3d112c5c137d9f12780c8409b2b96b81a9722c58
STEVEN_HOSKINS <steve.hoskins@theentertainer.com>
2025-08-19 14:12:17 +0100

    drop shadow to roundals

--- src/components/homeAgeRange/HomeAgeRange.js
+++ src/components/homeAgeRange/HomeAgeRange.js
@@ -9,43 +9,43 @@ const HomeAgeRange = () => {
         navigate("/category?age=0-3");
         window.scrollTo({top: 0,left: 0,behavior: "smooth",});
       }} className="transition-all lg:scale-95 rounded-full w-full aspect-square flex flex-col justify-center items-center bg-yellow-500 shadow-lg font-bold group hover:shadow-xl hover:scale-100 hover:rotate-[20deg] text-shadow-lg">
-        <span className="text-sm md:text-2xl lg:text-5xl text-white !leading-[1.2] md:!leading-[1]">0-3</span>
-        <span className="text-xs md:text-lg lg:text-3xl text-white leading-[1]">Years</span>
+        <span className="text-sm md:text-2xl lg:text-5xl text-white !leading-[1.2] md:!leading-[1] drop-shadow-lg">0-3</span>
+        <span className="text-xs md:text-lg lg:text-3xl text-white leading-[1] drop-shadow-lg">Years</span>
       </button>
       <button onClick={() => {
         navigate("/category?age=3-5");
         window.scrollTo({top: 0,left: 0,behavior: "smooth",});
       }} className="transition-all lg:scale-95 rounded-full w-full aspect-square flex flex-col justify-center items-center bg-red-500 shadow-lg font-bold group hover:shadow-xl hover:scale-100 hover:rotate-[20deg] text-shadow-lg">
-        <span className="text-sm md:text-2xl lg:text-5xl text-white !leading-[1.2] md:!leading-[1]">3-5</span>
-        <span className="text-xs md:text-lg lg:text-3xl text-white leading-[1]">Years</span>
+        <span className="text-sm md:text-2xl lg:text-5xl text-white !leading-[1.2] md:!leading-[1] drop-shadow-lg">3-5</span>
+        <span className="text-xs md:text-lg lg:text-3xl text-white leading-[1] drop-shadow-lg">Years</span>
       </button>
       <button onClick={() => {
         navigate("/category?age=5-8");
         window.scrollTo({top: 0,left: 0,behavior: "smooth",});
       }} className="transition-all lg:scale-95 rounded-full w-full aspect-square flex flex-col justify-center items-center bg-purple-600 shadow-lg font-bold group hover:shadow-xl hover:scale-100 hover:rotate-[20deg] text-shadow-lg">
-        <span className="text-sm md:text-2xl lg:text-5xl text-white !leading-[1.2] md:!leading-[1]">5-8</span>
-        <span className="text-xs md:text-lg lg:text-3xl text-white leading-[1]">Years</span>
+        <span className="text-sm md:text-2xl lg:text-5xl text-white !leading-[1.2] md:!leading-[1] drop-shadow-lg">5-8</span>
+        <span className="text-xs md:text-lg lg:text-3xl text-white leading-[1] drop-shadow-lg">Years</span>
       </button>
       <button onClick={() => {
         navigate("/category?age=8-11");
         window.scrollTo({top: 0,left: 0,behavior: "smooth",});
       }} className="transition-all lg:scale-95 rounded-full w-full aspect-square flex flex-col justify-center items-center bg-cyan-500 shadow-lg font-bold group hover:shadow-xl hover:scale-100 hover:rotate-[20deg] text-shadow-lg">
-        <span className="text-sm md:text-2xl lg:text-5xl text-white !leading-[1.2] md:!leading-[1]">8-11</span>
-        <span className="text-xs md:text-lg lg:text-3xl text-white leading-[1]">Years</span>
+        <span className="text-sm md:text-2xl lg:text-5xl text-white !leading-[1.2] md:!leading-[1] drop-shadow-lg">8-11</span>
+        <span className="text-xs md:text-lg lg:text-3xl text-white leading-[1] drop-shadow-lg">Years</span>
       </button>
       <button onClick={() => {
         navigate("/category?age=11-plus");
         window.scrollTo({top: 0,left: 0,behavior: "smooth",});
       }} className="transition-all lg:scale-95 rounded-full w-full aspect-square flex flex-col justify-center items-center bg-green-400 shadow-lg font-bold group hover:shadow-xl hover:scale-100 hover:rotate-[20deg] text-shadow-lg">
-        <span className="text-sm md:text-2xl lg:text-5xl text-white !leading-[1.2] md:!leading-[1]">11+</span>
-        <span className="text-xs md:text-lg lg:text-3xl text-white leading-[1]">Years</span>
+        <span className="text-sm md:text-2xl lg:text-5xl text-white !leading-[1.2] md:!leading-[1] drop-shadow-lg">11+</span>
+        <span className="text-xs md:text-lg lg:text-3xl text-white leading-[1] drop-shadow-lg">Years</span>
       </button>
       <button onClick={() => {
         navigate("/category?age=big-kids");
         window.scrollTo({top: 0,left: 0,behavior: "smooth",});
       }} className="transition-all lg:scale-95 rounded-full w-full aspect-square flex flex-col justify-center items-center bg-green-700 shadow-lg font-bold group hover:shadow-xl hover:scale-100 hover:rotate-[20deg] text-shadow-lg">
-        <span className="text-sm md:text-2xl lg:text-5xl text-white !leading-[1.2] md:!leading-[1]">Big</span>
-        <span className="text-xs md:text-lg lg:text-3xl text-white leading-[1]">Kids</span>
+        <span className="text-sm md:text-2xl lg:text-5xl text-white !leading-[1.2] md:!leading-[1] drop-shadow-lg">Big</span>
+        <span className="text-xs md:text-lg lg:text-3xl text-white leading-[1] drop-shadow-lg">Kids</span>
       </button>
     </div>
   )
